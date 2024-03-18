2feb88916133495427927abd625e949723d37be8
Nikodem Godek <nikodem.godek6@gmail.com>
2024-03-18 22:18:36 +0100

    In progress: Landing Page container 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.scss';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function App() {
   return (
@@ -7,21 +9,34 @@ function App() {
           <p>Nikodem.dev</p>
           <ul>
             <li>
-              <a href="#">Projects</a>
+              <a href="#linch">Projects</a>
             </li>
             <li>
-              <a href="#">Contact</a>
+              <a href="#xd">Contact</a>
             </li>
           </ul>
         </nav>
         <div className="container">
           <div className="about">
-            <p className="head-paragraph">React Developer</p>
-            <span className="aboutme-paragraph">Hi, I'm Nikodem Godek. A passionate React Developer based in Wrocław, Poland. 📍</span>
+            <div>
+              <h1 className="head-paragraph">React Developer</h1>
+              <span className="aboutme-paragraph">My name is Nikodem and I am a passionate React Developer based in Wrocław, Poland. 📍</span>
+            </div>
+            <div className="icons-container">
+              <div className="item">
+                <FaGithub size={30}/>
+              </div>
+              
+              <div className="item">
+                <FaLinkedinIn size={30}/>
+              </div>
+
+            </div>
+            
           </div>
           <div className="avatar-container">
             <div className="avatar">
-
+              <img alt="Woman" src={require('./resources/woman.jpg')} />
             </div>
           </div>
         </div>
