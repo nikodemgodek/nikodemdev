30fff9a4c61750330f71971f98e781c86be1eff1
Nikodem Godek <nikodem.godek6@gmail.com>
2024-03-18 19:46:08 +0100

    In progress: Navbar, Header aboutme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav className="navbar-container">
+          <p>Nikodem.dev</p>
+          <ul>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="container">
+          <div className="about">
+            <p className="head-paragraph">React Developer</p>
+            <span className="aboutme-paragraph">Hi, I'm Nikodem Godek. A passionate React Developer based in Wrocław, Poland. 📍</span>
+          </div>
+          <div className="avatar-container">
+            <div className="avatar">
+
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
